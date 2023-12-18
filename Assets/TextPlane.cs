4bf8e472099ab8d5bd9745d5f17e6a7e60f39cdb
raphaelBureau <94479381+raphaelBureau@users.noreply.github.com>
2023-12-18 18:51:10 -0500

    e to fly

--- Assets/TextPlane.cs
+++ Assets/TextPlane.cs
@@ -1,12 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TextPlane : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI tmp;
+    bool inside = false;
     void Update()
     {
-        transform.LookAt(Camera.main.transform); // Le text affichera toujours vers la caméra
-        transform.Rotate(0, 180, 0);
+        if (inside)
+        {
+            tmp.transform.LookAt(Camera.main.transform); // Le text affichera toujours vers la caméra
+            tmp.transform.Rotate(0, 180, 0);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene("Fly");
+                print("loading scene");
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        tmp.gameObject.SetActive(true);
+        inside = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        tmp.gameObject.SetActive(false);
+        inside = false;
     }
 }
